899fa05758be0ac8ba3c30c46c0f3faacf60873c
Mitchell Amihod <mitchell.amihod@gmail.com>
2013-03-13 11:39:35 -0400

    add charge and activate behaviour. Still have some work to do on flow and user messaging.

--- app/controllers/shopify.js
+++ app/controllers/shopify.js
@@ -2,9 +2,11 @@ require('../env');
 
 var everyauth = require('everyauth')
   ,Lang = require('yui').use().Lang
+  ,ShopifyAPI = require('node-shopify')
+  ,development = (process.env.NODE_ENV == 'development') ? true : false
   ;
 
-everyauth.debug = process.env.NODE_ENV == 'development' ? true : false;
+everyauth.debug = development;
 
 function authenticate(req, res) {
   var shop = req.body.shop
@@ -13,7 +15,7 @@ function authenticate(req, res) {
 
   if(undefined === shop || Lang.trim(shop) === '') {
     console.log('No Shop, redirect to login');
-    res.redirect('/login');
+    res.redirect('/');
   }
 
   shop = Lang.trim(shop.replace('http://', '').replace('https://',''));
@@ -35,5 +37,90 @@ function finalize(req, res) {
   res.redirect('/welcome');
 }
 
+
+//These should map to the plan types you offer.
+var plans = {
+  monthly: {
+    recurring_application_charge: {
+      name: "monthly"
+      ,price: "10.00"
+      ,test: development
+    }}
+  ,yearly: {
+    application_charge: {
+      name: "yearly"
+      ,price: "100.00"
+      ,test: development
+    }}
+};
+
+// Handle requests for being billed
+function charge(req, res) {
+  if(req.user === undefined) { res.redirect('/'); return; }
+  // console.log(req.user);
+  var user = req.user
+    , plan = plans[req.params.type]
+    , type = Object.keys(plan)[0]
+    ;
+
+  plan[type].return_url = 'http://' + req.headers.host + '/charge/activate?type='+type;
+
+  var client = new ShopifyAPI({
+    debug: true
+    ,version: "1.0.0"
+    ,host: user.shop.myshopify_domain
+    ,token: user.accessToken
+  });
+
+  client[type].create(plan, function(err, result) {
+    res.redirect(result[type].confirmation_url);
+  });
+}
+
+function activate(req, res) {
+  var charge_id = req.query.charge_id
+    , type = req.query.type
+    , user = req.user
+    ;
+
+  var client = new ShopifyAPI({
+    debug: true
+    ,version: "1.0.0"
+    ,host: user.shop.myshopify_domain
+    ,token: user.accessToken
+  });
+
+  client[type].one({id: charge_id}, function(err, result) {
+    var charge = result[type];
+
+    if(charge.status == 'accepted') {
+
+      var db = require('../lib/db/mongo.js');
+      db.collection('users', function(err, collection) {
+        if(err) { throw new Error(err); }
+
+        collection.update({_id: user._id},
+          {
+            $set: {
+              charge: {
+                id: charge.id,
+                type: type
+              }
+            }
+          });
+      });
+
+      client[type].activate({id: charge_id}, function() {
+        res.redirect('/welcome');
+      });
+    }
+    else {
+      res.redirect('/welcome');
+    }
+  });
+}
+
 exports.authenticate = authenticate;
 exports.finalize = finalize;
+exports.charge = charge;
+exports.activate = activate;
